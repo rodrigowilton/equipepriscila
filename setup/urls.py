e87d0522eae286bbe65f262773ececfdd406cb49
unknown <you@example.com>
2024-09-01 10:16:48 -0300

    equipadoca

--- setup/urls.py
+++ setup/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),  # Associa a view 'index' ao nome 'index'
     path('app/', include('app.urls')),  # Inclui as URLs do seu aplicativo (substitua 'app' pelo nome do seu aplicativo)
+    
 ]
